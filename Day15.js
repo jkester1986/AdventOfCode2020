1af0e173000811edaa73d4b963eab34ab16a73cb
Julia Kester <kester@oswego.edu>
2020-12-15 21:01:12 -0500

    Day 15 optimizations

--- Day15.js
+++ Day15.js
@@ -1,43 +1,22 @@
-let numbers = [9,19,1,6,0,5,4],
-	numbersAndTurns = {};
+// This is version #2, after looking at someone else's solution
+// and realizing I could simplify things a bit
+// Also, I discovered js has Map()'s, and they're faster than a standard object (11 mins down to 5s)
+let numbers = [9,19,1,6,0,5], // take out the last value and use it as currentNumber below
+	numbersAndTurns = new Map();
 
 numbers.forEach((number, i) => {
-	numbersAndTurns[number] = {
-		latestTurn: i
-	};
+	numbersAndTurns.set(number, i);
 });
 
-
-let previousNumber = numbers[numbers.length - 1];
+let currentNumber = 4;
 for (let i = numbers.length; i < 30000000; i++) {
+	let val = numbersAndTurns.get(currentNumber);
+	let next = val == undefined ? 0 : i - val;
+	numbersAndTurns.set(currentNumber, i);
 
-	// previous number did not exist before
-	if (numbersAndTurns[`${previousNumber}`]?.previousTurn === undefined) {
-		numbersAndTurns['0'] = {
-			previousTurn: numbersAndTurns['0'].latestTurn,
-			latestTurn: i
-		};
-		previousNumber = 0;
+	if (i === 2019) console.log("P1:", currentNumber);
+	if (i === 30000000 - 1) { //oh boy. Well, this isn't exactly optimized....but it also doesn't take an absolute eternity
+		console.log("P2:", currentNumber);
 	}
-	// previous number did exist before
-	else {
-		let difference = numbersAndTurns[`${previousNumber}`].latestTurn - numbersAndTurns[`${previousNumber}`].previousTurn;
-		if(numbersAndTurns[`${difference}`]) {
-			numbersAndTurns[`${difference}`] = {
-				previousTurn: numbersAndTurns[`${difference}`].latestTurn,
-				latestTurn: i
-			};
-		}
-		else {
-			numbersAndTurns[`${difference}`] = { latestTurn: i };
-		}
-		
-		previousNumber = difference;
-
-
-		if (i === 2019) console.log("P1:", difference);
-		if (i === 30000000 - 1) { //oh boy. Well, this isn't exactly optimized....but it also doesn't take an absolute eternity
-			console.log("P2:", difference);
-		}
-	}
+	currentNumber = next;
 }
